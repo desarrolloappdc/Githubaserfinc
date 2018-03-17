a0b89f58feb065956f83e1daf3866325e367d396
lion3214 <37456790+lion3214@users.noreply.github.com>
2018-03-17 09:35:05 -0500

    accion de botones
    se ponen las acciones delas visas

--- menu.php
+++ menu.php
@@ -17,19 +17,19 @@
   	</a>
 
 
-  	<a href="home.php" target="vista" >
+  	<a href="administrador.php" target="vista" >
   		<button class="btn waves-effect waves-light col s11" type="submit" name="action" >administrador
     		<i class="material-icons right">send</i>
   		</button>
   	</a>
 
-  	<a href="home.php" target="vista" >
+  	<a href="recursos_humenos.php" target="vista" >
   		<button class="btn waves-effect waves-light col s11" type="submit" name="action">recursos humanos
     		<i class="material-icons right">send</i>
   		</button>
   	</a>
 
-  	<a href="home.php" target="vista" >
+  	<a href="tecnicos.php" target="vista" >
   		<button class="btn waves-effect waves-light col s11" type="submit" name="action">tecnicos
     		<i class="material-icons right">send</i>
   		</button>
@@ -41,13 +41,13 @@
   		</button>
   	</a>
 
-  	<a href="home.php" target="vista" >
+  	<a href="reportes.php" target="vista" >
   		<button class="btn waves-effect waves-light col s11" type="submit" name="action">reportes
     		<i class="material-icons right">send</i>
   		</button>
   	</a>
 
-  	<a href="home.php" target="vista" >
+  	<a href="tikets.php" target="vista" >
   		<button class="btn waves-effect waves-light col s11" type="submit" name="action">tikets
     	<i class="material-icons right">send</i>
   		</button>
