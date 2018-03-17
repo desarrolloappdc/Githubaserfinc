c0fbba8b492b0372f15bd319c60cafd22dbe4de3
lion3214 <37456790+lion3214@users.noreply.github.com>
2018-03-17 09:31:03 -0500

    agregando botones
    creacion de botones y de archivos de la vista

--- menu.php
+++ menu.php
@@ -2,14 +2,58 @@
 <html>
 <head>
 	<title></title>
-</head>
-<style type="text/css">
-	*{
-		padding: 0;
-		margin: 0;
-	}
-</style>
-<body style="background-color: gray">
+	<!-- Compiled and minified CSS -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
 
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+</head>
+
+<body style="background-color: gray" class="row">
+
+	<a href="home.php" target="vista" >
+		<button class="btn waves-effect waves-light col s11" type="submit" name="action"  >login
+    		<i class="material-icons right">send</i>
+  		</button>
+  	</a>
+
+
+  	<a href="home.php" target="vista" >
+  		<button class="btn waves-effect waves-light col s11" type="submit" name="action" >administrador
+    		<i class="material-icons right">send</i>
+  		</button>
+  	</a>
+
+  	<a href="home.php" target="vista" >
+  		<button class="btn waves-effect waves-light col s11" type="submit" name="action">recursos humanos
+    		<i class="material-icons right">send</i>
+  		</button>
+  	</a>
+
+  	<a href="home.php" target="vista" >
+  		<button class="btn waves-effect waves-light col s11" type="submit" name="action">tecnicos
+    		<i class="material-icons right">send</i>
+  		</button>
+  	</a>
+
+  	<a href="noticias.php" target="vista" >
+  		<button class="btn waves-effect waves-light col s11" type="submit" name="action">noticias
+    		<i class="material-icons right">send</i>
+  		</button>
+  	</a>
+
+  	<a href="home.php" target="vista" >
+  		<button class="btn waves-effect waves-light col s11" type="submit" name="action">reportes
+    		<i class="material-icons right">send</i>
+  		</button>
+  	</a>
+
+  	<a href="home.php" target="vista" >
+  		<button class="btn waves-effect waves-light col s11" type="submit" name="action">tikets
+    	<i class="material-icons right">send</i>
+  		</button>
+  	</a>
+  	
+<!-- Compiled and minified JavaScript -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
 </body>
 </html>
